2671b1f2122a667cbcc2e36b6eccc3f5dcbcc42c
Beatriz Ferreira <biatizferreira04@gmail.com>
2023-10-27 03:56:29 +0100

    Started Operation class

---
+++ code/Operation.cpp
@@ -0,0 +1,6 @@
+//
+// Created by AnaBeatrizCarneiroFe on 27/10/2023.
+//
+
+#include "Operation.h"
+
---
+++ code/Operation.h
@@ -0,0 +1,23 @@
+//
+// Created by AnaBeatrizCarneiroFe on 27/10/2023.
+//
+
+#ifndef AED2324_PRJ1_G11_OPERATION_H
+#define AED2324_PRJ1_G11_OPERATION_H
+
+#include <queue>
+#include "Log.h"
+#include "Turma.h"
+
+class Operation {
+private:
+    std::queue<std::string> requests;
+public:
+
+    Operation();
+
+    //I'll do the rest tomorrow *_*
+};
+
+
+#endif //AED2324_PRJ1_G11_OPERATION_H
